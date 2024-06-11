59de9b042c3b5f2689deeb127d8509c8f738b3c8
gwagjiug <kwjo0228@naver.com>
2024-06-11 09:32:38 +0900

    2024-06-11

---
+++ src/java_0611/If_test.java
@@ -0,0 +1,15 @@
+package java_0611;
+
+//Chap 04
+
+public class If_test {
+    public static void main(String[] args) {
+
+        int hour = 9;
+
+        if(hour <12){
+            System.out.println("Okay i'm fine");
+        }
+
+    }
+}
