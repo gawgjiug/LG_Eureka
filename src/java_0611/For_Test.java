3a391bfe7299d1dc1bafd4f6072a39356ece77e6
gwagjiug <kwjo0228@naver.com>
2024-06-11 10:46:07 +0900

    2024-06-11

--- src/java_0611/For_Test.java
+++ src/java_0611/For_Test.java
@@ -3,5 +3,15 @@ package java_0611;
 public class For_Test {
     public static void main(String[] args) {
 
+        int sum = 0;
+        for (int i = 0; i<=5; i++){
+            sum += i;
+        }
+
+        System.out.println(sum);
+        /*자바는 block 단위로 변수를 관리한다. 블록은 중괄호를 말함
+        * 선언한 블록을 벗어나게 되면 해당 변수는 사라짐
+        * */
+
     }
 }
---
+++ src/java_0611/For_Test_2.java
@@ -0,0 +1,22 @@
+package java_0611;
+
+public class For_Test_2 {
+    public static void main(String[] args) {
+
+        //짝수의 합
+
+        int sum = 0;
+        for (int i = 0; i<=5; i++){
+
+            if(i % 2 == 0)
+                sum += i;
+
+//            sum -= i; if 문에 코드가 한줄이면 중괄호 없앨 수 있고 밑에 있는 코드는 if 문 지나고 무조건 실행함
+
+        }
+
+        System.out.println(sum);
+
+
+    }
+}
